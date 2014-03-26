7f308e2affba6a0ecc0ca37b29145be513539481
smialyjoe <smialyjoe@gmail.com>
2014-03-26 08:51:24 +0100

    Testowanie czego do tej pory zrobiłem

--- lab5/lab5/lab5.cpp
+++ lab5/lab5/lab5.cpp
@@ -3,19 +3,41 @@
 
 #include "stdafx.h"
 #include "Czworokat.h"
+#include "Trojkat.h"
 
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+
+	int rozmiar = 2;
+	Figura **figury = new Figura*[rozmiar];
+
 	Czworokat cz1 = Czworokat();
 	cz1.setPoint(0, 2, 0);
 	cz1.setPoint(2, 2, 1);
 	cz1.setPoint(2, 0, 2);
 	cz1.setPoint(0, 0, 3);
 
-	cout << cz1.ToString() << endl;
+	figury[0] = &cz1;
 
+	Trojkat t = Trojkat();
+	t.setPoint(0, 0, 0);
+	t.setPoint(3, 3, 1);
+	t.setPoint(6, 0, 2);
+
+	figury[1] = &t;
+
+	for (int i = 0; i < rozmiar; i++)
+	{
+		cout << figury[i]->ToString() << endl;
+		cout << "obwod: " << figury[i]->getObwod() << endl;
+	}
+	
 	system("pause");
+
+	delete[] figury;
+	figury = NULL;
+
 	return 0;
 }
 
